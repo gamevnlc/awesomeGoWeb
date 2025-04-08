c16569a5117e579adb5cb4afe7b41059c0aeedf2
Admin <huypx@outlook.com>
2025-04-08 21:00:58 +0700

    06-03 enable static file

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,6 +20,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Use(SessionLoad)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
