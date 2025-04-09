1ca45fbfb46c52ef3e6c47dd334d8d13c8e59c8d
Pham Xuan Huy <huypx@gearinc.com>
2025-04-09 18:05:07 +0700

    08-007 11:36

---
+++ internal/form/errors.go
@@ -0,0 +1,18 @@
+package form
+
+type errors map[string][]string
+
+// Add adds an error message
+func (e errors) Add(field string, message string) {
+	e[field] = append(e[field], message)
+}
+
+// Get return the first error message
+func (e errors) Get(field string) string {
+	es := e[field]
+	if len(es) == 0 {
+		return ""
+	}
+
+	return es[0]
+}
