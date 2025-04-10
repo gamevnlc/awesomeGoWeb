1704d120d2477b66b0c945cc4e39f6b631c54568
gamevnlc <gamevnlc1591@gmail.com>
2025-04-10 22:40:42 +0700

    13-009 Inserting Room Restrictions

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -11,16 +11,17 @@ func (m *postgresDBRepo) AllUsers() bool {
 }
 
 // InsertReservation insert reservation into database
-func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
+func (m *postgresDBRepo) InsertReservation(res models.Reservation) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	defer cancel()
 
+	var newID int
 	//goland:noinspection SqlDialectInspection,SqlNoDataSourceInspection
 	stmt := `insert into reservations (first_name, last_name, email, phone, 
                           start_date, end_date, room_id, created_at, updated_at)
                           values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id`
 
-	_, err := m.DB.ExecContext(ctx, stmt,
+	err := m.DB.QueryRowContext(ctx, stmt,
 		res.FirstName,
 		res.LastName,
 		res.Email,
@@ -30,8 +31,32 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 		res.RoomID,
 		time.Now(),
 		time.Now(),
-	)
+	).Scan(&newID)
 
+	if err != nil {
+		return 0, err
+	}
+
+	return newID, nil
+}
+
+// InsertRoomRestriction insert a room restriction into database
+func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	//goland:noinspection SqlDialectInspection,SqlNoDataSourceInspection
+	stmt := `insert into room_restrictions (start_date, end_date, room_id, reservation_id, restriction_id, 
+                               created_at, updated_at) values ($1, $2, $3, $4, $5, $6, $7)`
+
+	_, err := m.DB.ExecContext(ctx, stmt,
+		r.StartDate,
+		r.EndDate,
+		r.RoomID,
+		r.ReservationID,
+		r.RestrictionID,
+		time.Now(),
+		time.Now(),
+	)
 	if err != nil {
 		return err
 	}
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,5 +5,6 @@ import "awesomeWeb/internal/models"
 type DatabaseRepo interface {
 	AllUsers() bool
 
-	InsertReservation(res models.Reservation) error
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(res models.RoomRestriction) error
 }
