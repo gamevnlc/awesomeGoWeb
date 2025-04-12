ff5ecd0cd16da91c6948414ffe759fee61845ac3
gamevnlc <gamevnlc1591@gmail.com>
2025-04-12 09:31:40 +0700

    13-020 Adding a migration for seeding rooms

---
+++ migrations/20250412022956_create_seed_rooms_tables.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO "public"."rooms" VALUES (1, 'General', '2025-04-10 22:21:12', '2025-04-10 22:21:18');
+INSERT INTO "public"."rooms" VALUES (2, 'Major', '2025-04-10 22:21:12', '2025-04-10 22:21:18');
