e5e6780cc0322f1b98558dd110074c504200fd37
gamevnlc <gamevnlc1591@gmail.com>
2025-04-12 10:59:53 +0700

    16-002 Creating the authentication handlers for the login screen

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -356,3 +356,9 @@ func (m *Repository) BookRoom(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/make-reservation", http.StatusSeeOther)
 
 }
+
+func (m *Repository) ShowLogin(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "login.page.tmpl", &models.TemplateData{
+		Form: form.New(nil),
+	})
+}
