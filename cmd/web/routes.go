519ee02867496ad9df5fa38e4801fc60c5fe551b
gamevnlc <gamevnlc1591@gmail.com>
2025-04-13 10:41:21 +0700

    16-008 Checking to see if a user is logged in, and logging a user out

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -42,6 +42,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/user/login", handlers.Repo.ShowLogin)
 	mux.Post("/user/login", handlers.Repo.PostShowLogin)
+	mux.Get("/user/logout", handlers.Repo.Logout)
 
 	return mux
 
--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -4,13 +4,14 @@ import "awesomeWeb/internal/form"
 
 // TemplateData holds data sent from handlers to template
 type TemplateData struct {
-	StringMap map[string]string
-	IntMap    map[string]int
-	FloatMap  map[string]float32
-	Data      map[string]interface{}
-	CSRFToken string
-	Flash     string
-	Warning   string
-	Error     string
-	Form      *form.Form
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float32
+	Data            map[string]interface{}
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	Form            *form.Form
+	IsAuthenticated int
 }
