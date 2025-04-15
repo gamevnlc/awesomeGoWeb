3d5bad47a55303f31755d92e18657b7e657333cb
gamevnlc <gamevnlc1591@gmail.com>
2025-04-15 22:32:16 +0700

    17-016 Reservation Calendar IV

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -499,17 +499,18 @@ func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Re
 
 		for _, y := range restrictions {
 			if y.ReservationID > 0 {
+
 				//it's a reservation
 				for d := y.StartDate; d.After(y.EndDate) == false; d = d.AddDate(0, 0, 1) {
 					reservationMap[d.Format("2006-01-2")] = y.ReservationID
 				}
 			} else {
 				//it's a block
-				blockMap[y.StartDate.Format("2006-01-2")] = y.ReservationID
+				blockMap[y.StartDate.Format("2006-01-2")] = y.ID
 			}
 		}
 		data[fmt.Sprintf("reservation_map_%d", x.ID)] = reservationMap
-		data[fmt.Sprintf("block_map_%d", x.ID)] = reservationMap
+		data[fmt.Sprintf("block_map_%d", x.ID)] = blockMap
 
 		m.App.Session.Put(r.Context(), fmt.Sprintf("block_map_%d", x.ID), blockMap)
 	}
